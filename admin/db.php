046d8e2ea3825192dcf93481c877d040cf3fc23b
Ezrah K <kiiluezra@gmail.com>
2025-09-04 15:25:19 +0300

    Restore local XAMPP database settings for development

--- admin/db.php
+++ admin/db.php
@@ -1,6 +1,5 @@
 <?php
-// InfinityFree Database Configuration
-$con = mysqli_connect("sql300.infinityfree.com", "if0_39842749", "6LLK5O9akZAKiZi", "if0_39842749_XXX") or die(mysql_error());
+$con = mysqli_connect("localhost","root","","hotel") or die(mysql_error());
 
 // Set charset to utf8mb4 for proper emoji support
 mysqli_set_charset($con, "utf8mb4");
